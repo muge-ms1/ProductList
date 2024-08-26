8ae39b9cfffda59a60a62798ddd84684493e54fa
muge-ms1 <murugan63833@gmail.com>
2024-08-26 20:40:15 +0530

    dark theme updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import { useState } from 'react';
+import './product/Cards.css';
 import Cards from './product/Cards';
 import ProductDetail from './product/ProductDetail';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Header from './Components/Header';
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+        document.body.className = isDarkTheme ? '' : 'dark-theme';
+    };
+
   return (
+    
     <div className="App">
       <BrowserRouter>
       <Header />
+      <button onClick={toggleTheme} className="btn btn-primary mb-3">
+                {isDarkTheme ? 'Light' : 'Dark'} Theme
+            </button>
             <Routes>
                 <Route path="/" element={<Cards />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
--- src/product/ProductDetail.js
+++ src/product/ProductDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
-
+import './Cards.css'
 const ProductDetail = () => {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
